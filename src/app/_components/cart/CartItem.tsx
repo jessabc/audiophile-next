2e6cff49cae724d3939992f6c7882164def2cc84
jess <120062852+jessabc@users.noreply.github.com>
2024-04-19 13:32:33 +1000

    fix undefined

--- src/app/_components/cart/CartItem.tsx
+++ src/app/_components/cart/CartItem.tsx
@@ -1,29 +1,34 @@
-import { ProductInCart } from '@/app/interfaces';
-import Image from 'next/image';
+import { ProductInCart } from "@/app/interfaces";
+import Image from "next/image";
 
 interface Props {
-    product: ProductInCart
-  }
-  
-export default function CartItem({product}: Props) {
+  product: ProductInCart;
+}
 
-    return (
-        <>
-            <div>
-                <Image
-                    src={`/${product?.image?.mobile}`} 
-                    width="0"
-                    height="0"
-                    sizes="100vw"
-                    className="w-full h-auto rounded-lg"
-                    alt={product?.name}
-                />
-                
-            </div>
-            <div className="col-span-2">
-                <p className="font-bold leading-6 text-black">{product?.name}</p>
-                <p className="font-bold text-sm leading-6 text-black opacity-50">$ {(new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(product?.price))}</p> 
-            </div>
-        </>
-    )
+export default function CartItem({ product }: Props) {
+  return (
+    <>
+      {product && (
+        <div>
+          <Image
+            src={`${product?.image?.mobile}`}
+            width="0"
+            height="0"
+            sizes="100vw"
+            className="w-full h-auto rounded-lg"
+            alt={product?.name}
+          />
+        </div>
+      )}
+      <div className="col-span-2">
+        <p className="font-bold leading-6 text-black">{product?.name}</p>
+        <p className="font-bold text-sm leading-6 text-black opacity-50">
+          ${" "}
+          {new Intl.NumberFormat("en-IN", {
+            maximumSignificantDigits: 3,
+          }).format(product?.price)}
+        </p>
+      </div>
+    </>
+  );
 }
