6589a37b86b2bc29ff1b7f6c879b848810cad9e6
jess <120062852+jessabc@users.noreply.github.com>
2024-04-19 13:32:18 +1000

    prettier

--- src/app/_components/menuInModal/MenuInModal.tsx
+++ src/app/_components/menuInModal/MenuInModal.tsx
@@ -1,33 +1,31 @@
-import CategoryInMenuModal from './CategoryInMenuModal'
+import CategoryInMenuModal from "./CategoryInMenuModal";
 
 interface Props {
-    setOpen:  React.Dispatch<React.SetStateAction<boolean>>;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function MenuInModal
-({setOpen}: Props) {
+export default function MenuInModal({ setOpen }: Props) {
+  return (
+    <section
+      className={` gap-5  pt-14 flex flex-col md:grid md:grid-cols-3 md:gap-2`}
+    >
+      <CategoryInMenuModal
+        image="/assets/shared/desktop/image-category-thumbnail-headphones.png"
+        link="headphones"
+        setOpen={setOpen}
+      />
 
-    return (
-        <section className={` gap-5  pt-14 flex flex-col md:grid md:grid-cols-3 md:gap-2`}  > 
- 
-                <CategoryInMenuModal 
-                    image='/assets/shared/desktop/image-category-thumbnail-headphones.png' 
-                    link='headphones'
-                    setOpen={setOpen}
-                />
+      <CategoryInMenuModal
+        image="/assets/shared/desktop/image-category-thumbnail-speakers.png"
+        link="speakers"
+        setOpen={setOpen}
+      />
 
-                <CategoryInMenuModal 
-                    image='/assets/shared/desktop/image-category-thumbnail-speakers.png'
-                    link='speakers'
-                    setOpen={setOpen}
-                />
-
-                <CategoryInMenuModal 
-                    image='/assets/shared/desktop/image-category-thumbnail-earphones.png' 
-                    link='earphones'
-                    setOpen={setOpen}
-                />
-
-            </section>
-    )
+      <CategoryInMenuModal
+        image="/assets/shared/desktop/image-category-thumbnail-earphones.png"
+        link="earphones"
+        setOpen={setOpen}
+      />
+    </section>
+  );
 }
--- src/app/_components/product/OtherProductYouMayLike.tsx
+++ src/app/_components/product/OtherProductYouMayLike.tsx
@@ -1,64 +1,65 @@
-import Image from 'next/image'
-import { useRouter } from "next/navigation"
-import { IProduct, Other } from '../../interfaces'
-import useGetProducts from '@/app/_hooks/useGetProducts'
-
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { IProduct, Other } from "../../interfaces";
+import useGetProducts from "@/app/_hooks/useGetProducts";
 
 interface Props {
-    product: Other
+  product: Other;
 }
 
-export default function OtherProductYouMayLike({product}: Props) {
+export default function OtherProductYouMayLike({ product }: Props) {
+  const router = useRouter();
 
-    const router = useRouter()
+  const { image, name, slug } = product;
 
-    const {image, name, slug} = product
+  const products = useGetProducts();
 
-    const products = useGetProducts()
+  function seeProduct() {
+    const thisProduct = products.find(
+      (item: IProduct) => item.slug === product.slug
+    );
+    router.push(`/${thisProduct?.category}/${thisProduct?.slug}`);
+  }
 
-    function seeProduct() {
-        const thisProduct = products.find((item: IProduct) => item.slug === product.slug)
-        router.push(`/${thisProduct?.category}/${thisProduct?.slug}`)
-    }
-   
-   
-    return (
-        <div className='flex flex-col gap-5 justify-center items-center mb-12'>
+  return (
+    <div className="flex flex-col gap-5 justify-center items-center mb-12">
+      <Image
+        src={`${image.mobile}`}
+        width="0"
+        height="0"
+        sizes="100vw"
+        className="w-full h-auto md:hidden lg:hidden rounded-lg "
+        alt={`${name} image`}
+      />
+      <Image
+        src={`${image.tablet}`}
+        width="0"
+        height="0"
+        sizes="100vw"
+        className="w-full h-auto hidden md:block lg:hidden rounded-lg"
+        alt={`${name} image`}
+      />
+      <Image
+        src={`${image.desktop}`}
+        width="0"
+        height="0"
+        sizes="100vw"
+        className="w-full h-auto hidden md:hidden lg:block rounded-lg"
+        alt={`${name} image`}
+      />
 
-            <Image
-                src={`${image.mobile}`}
-                width="0"
-                height="0"
-                sizes="100vw"
-                className="w-full h-auto md:hidden lg:hidden rounded-lg "
-                alt={`${name} image`} 
-            />
-            <Image
-                src={`${image.tablet}`}
-                width="0"
-                height="0"
-                sizes="100vw"
-                className="w-full h-auto hidden md:block lg:hidden rounded-lg"
-                alt={`${name} image`} 
-            />
-            <Image
-                src={`${image.desktop}`}
-                width="0"
-                height="0"
-                sizes="100vw"
-                className="w-full h-auto hidden md:hidden lg:block rounded-lg"
-                alt={`${name} image`} 
-            />
+      <p className="font-bold text-2xl text-center tracking-wide uppercase text-black">
+        {name}
+      </p>
 
-            <p className='font-bold text-2xl text-center tracking-wide uppercase text-black'>{name}</p>
-
-            <div>
-                <button  
-                    className='font-bold text-sm leading-5 tracking-wide uppercase text-white bg-orange w-40 h-12 hover:bg-lightOrange'  
-                    onClick={seeProduct}
-                    >see product
-                </button>  
-            </div>
-        </div>
-    )
+      <div>
+        <button
+          className="font-bold text-sm leading-5 tracking-wide uppercase text-white bg-orange w-40 h-12 hover:bg-lightOrange"
+          onClick={seeProduct}
+        >
+          see product
+        </button>
+      </div>
+    </div>
+  );
 }
